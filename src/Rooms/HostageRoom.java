90a2c8f512aee716ed16ee2a86281680edfcc061
BT_1N3_26@bktech.local <BT_1N3_26@bktech.local>
2018-11-07 10:16:45 -0500

    Initial commit

---
+++ src/Rooms/HostageRoom.java
@@ -0,0 +1,24 @@
+package Rooms;
+import Game.Runner;
+import People.Person;
+
+public class HostageRoom extends Room
+{
+    public HostageRoom(int x, int y)
+    {
+        super(x, y);
+    }
+
+    /**
+     * Triggers the game ending conditions.
+     * @param x the Person entering
+     */
+    @Override
+    public void enterRoom(Person x)
+    {
+        occupant = x;
+        x.setxLoc(this.xLoc);
+        x.setyLoc(this.yLoc);
+        System.out.println("You found the HostageRoom! Now go to the Secret Entrance");
+    }
+}
---
+++ src/Rooms/WeaponRoom.java
@@ -0,0 +1,25 @@
+package Rooms;
+import People.*;
+import Game.Runner;
+
+public class WeaponRoom extends Room
+{
+
+    public WeaponRoom(int x, int y)
+    {
+        super(x, y);
+    }
+
+    /**
+     * Triggers the game ending conditions.
+     * @param x the Person entering
+     */
+    @Override
+    public void enterRoom(Person x)
+    {
+        occupant = x;
+        x.setxLoc(this.xLoc);
+        x.setyLoc(this.yLoc);
+        System.out.println("You found the HostageRoom! Now go to the Secret Entrance");
+    }
+}
